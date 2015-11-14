714551a2c1f2129a3c61ed297c555a0b01c154be
ukiy6969 <ukiy6969@gmail.com>
2015-11-14 18:10:01 +0900

    ok show float button text

--- tsubasa/src/main/java/co/komachan/tsubasa/TsubasaService.java
+++ tsubasa/src/main/java/co/komachan/tsubasa/TsubasaService.java
@@ -32,45 +32,22 @@ public class TsubasaService extends Service {
         sideSwipeView = new SideSwipeView(this, wm);
         final FloatMovalButton fmbtn = new FloatMovalButton(this, wm, 0, 0, 400, 300);
         sideSwipeView.show();
-        sideSwipeView.setOnClickListener(new View.OnClickListener() {
+        sideSwipeView.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View v) {
-                fmbtn.show();
-                sideSwipeView.hide();
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        handler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                sideSwipeView.hide();
+                                fmbtn.show();
+                            }
+                        });
+                }
+                return false;
             }
         });
-        /*
-        view.setBackgroundColor(Color.RED);
-        clickBtn = (Button) li.inflate(R.layout.float_moval_button, null);
-        clickBtn.setVisibility(View.INVISIBLE);
-        clickBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                wm.removeView(clickBtn);
-            }
-        });
-
-        params = new WindowManager.LayoutParams(
-                400, 300,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                    WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
-                PixelFormat.RGB_565
-        );
-        view.setOnTouchListener(new View.OnTouchListener() {
-
-            /*
-            handler.post(() -> {
-                TextView tv =(TextView) view.findViewById(R.id.textView1);
-                tv.setText("Click");
-                wm.removeView(view);
-            });
-            */
-        /*
-        });
-        wm.addView(view, lparams);
-        */
         return START_STICKY;
     }
 
